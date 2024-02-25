2119458e6c5d643768b7eed9b58c08a1147f788c
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2024-02-25 17:10:53 -0300

    function verificaChute()

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 let numeroSecreto = gerarNumeroAleatorio();
+console.log(numeroSecreto);
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -10,8 +11,9 @@ exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 
-function varificarChute() {
-
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
 }
 
 function gerarNumeroAleatorio() {
